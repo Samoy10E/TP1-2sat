0553570704768e38680b02222257a9be789e5e68
Samoy10E <90957900+Samoy10E@users.noreply.github.com>
2021-09-18 11:15:23 +0200

    Fini Lecture Fichier

--- src/LectureFichier.java
+++ src/LectureFichier.java
@@ -7,7 +7,7 @@ public class LectureFichier {
         Lecture(nom);
     }
 
-    private void Lecture(String nom) throws IOException {
+    private ArrayList<String[]> Lecture(String nom) throws IOException {
         String nomFichier = "formule/";
         nomFichier = nomFichier.concat(nom);
         nomFichier = nomFichier.concat(".txt");
@@ -18,17 +18,14 @@ public class LectureFichier {
 
         String readLine;
 
-        ArrayList<String> list = new ArrayList<>();
+        ArrayList<String[]> Clauses = new ArrayList<>();
 
         while ((readLine = b.readLine()) != null) {
             if (readLine.charAt(0) == 'c'){break;}
-            else if (readLine.charAt(0) == 'p'){
-                String[] mots = readLine.split(" ");
-                int Nvar = Integer.parseInt(mots[2]);
-                int Nclause = Integer.parseInt(mots[3]);
-                System.out.println(Nvar);
-                System.out.println(Nclause);
+            else{
+                Clauses.add(readLine.split(" "));
             }
         }
+        return Clauses;
     }
 }
