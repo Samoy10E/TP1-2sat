b9337bf0d283319df425a827718cc6d41af1c5ff
Samoy10E <90957900+Samoy10E@users.noreply.github.com>
2021-09-23 21:51:19 +0200

    Fini

--- src/TraitementGraphe.java
+++ src/TraitementGraphe.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 public class TraitementGraphe {
 
     public static ArrayList<String> LectureFichier(String nom) throws IOException {
+        //Création du chemin relatif "formule/nomFichier.txt"
         String nomFichier = "formule/";
         nomFichier = nomFichier.concat(nom);
         nomFichier = nomFichier.concat(".txt");
@@ -16,9 +17,9 @@ public class TraitementGraphe {
         BufferedReader b = new BufferedReader(new FileReader(file));
 
         String readLine;
-
         ArrayList<String> Clauses = new ArrayList<>();
 
+        //Remplissage de l'ArrayList
         while ((readLine = b.readLine()) != null) {
             Clauses.add(readLine);
         }
@@ -26,17 +27,24 @@ public class TraitementGraphe {
     }
 
     public static Graph CreationGraph(ArrayList<String> Clauses){
+        //On cherche la ligne de paramètre, on va supposer qu'il y en a qu'une
         int i=0;
         while (i<Clauses.size() && !Clauses.get(i).contains("p ")){i++;}
 
+        //On récupère les informations
         String[] param = Clauses.get(i).split(" ");
         int Nvar = Integer.parseInt(param[2]);
         int Nclause = Integer.parseInt(param[3]);
         Clauses.remove(i);
 
+        /*
+        Initialisations du graphs, il y a 2 fois plus de sommet que de variable
+        car il y aura x et -x, la variable et sa négation
+         */
        Graph graph = new Graph(Nvar*2);
 
        int source,dest;
+       String[] mots;
        for (i=0;i<Clauses.size();i++){
            String ligne = Clauses.get(i);
            if (ligne.contains("c ")){
@@ -48,13 +56,17 @@ public class TraitementGraphe {
                }
            }
            else {
-               String[] mots = ligne.split(" ");
-               source = convVarToIndex(Integer.parseInt(mots[0]),Nvar*2);
-               dest = convVarToIndex(-Integer.parseInt(mots[1]),Nvar*2);
+               //Je décompose la ligne en plusieurs mot, ex "x y 0"->["x","y","0"]
+               mots = ligne.split(" ");
+
+               // source = -x; destination = y
+               source = convVarToIndex(-Integer.parseInt(mots[0]),Nvar*2);
+               dest = convVarToIndex(Integer.parseInt(mots[1]),Nvar*2);
                graph.addArc(source,dest,"");
 
-               dest = convVarToIndex(-Integer.parseInt(mots[0]),Nvar*2);
-               source = convVarToIndex(Integer.parseInt(mots[1]),Nvar*2);
+               //source = -y; destination = x
+               source = convVarToIndex(-Integer.parseInt(mots[1]),Nvar*2);
+               dest = convVarToIndex(Integer.parseInt(mots[0]),Nvar*2);
                graph.addArc(source,dest,"");
            }
        }
